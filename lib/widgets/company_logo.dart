912a20c64238b43f0321f45a0d24507355b9ae2c
Vladyslav Kramarenko <vladyslavkramarenko@MacBook-Pro-Vlad.local>
2022-01-12 18:09:38 +0200

    add svg support

--- lib/widgets/company_logo.dart
+++ lib/widgets/company_logo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CompanyLogo extends StatelessWidget {
   final String mainImage;
@@ -9,6 +10,30 @@ class CompanyLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(mainImage);
+    bool secondaryProvided = secondaryImage != null;
+    bool isMainSvg = RegExp(r"\.svg").allMatches(mainImage).isNotEmpty;
+    bool isSecondSvg = false;
+    if (secondaryProvided) {
+      isSecondSvg = RegExp(r"\.svg").allMatches(secondaryImage!).isNotEmpty;
+    }
+    return Stack(
+      alignment: Alignment.bottomRight,
+      children: [
+        Center(child: isMainSvg ? SvgPicture.asset(mainImage) : Image.asset(mainImage)),
+        secondaryProvided
+            ? isSecondSvg
+                ? SvgPicture.asset(
+                    secondaryImage!,
+                    width: 50,
+                    height: 50,
+                  )
+                : Image.asset(
+                    secondaryImage!,
+                    width: 50,
+                    height: 50,
+                  )
+            : Container()
+      ],
+    );
   }
 }
